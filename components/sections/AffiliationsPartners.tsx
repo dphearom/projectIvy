239c9bb21f1aee05e50371c0582c77d2582bea37
Makara Teu <teu@aggdev01.onmicrosoft.com>
2026-07-07 13:08:24 +0700

    add schools logo

--- components/sections/AffiliationsPartners.tsx
+++ components/sections/AffiliationsPartners.tsx
@@ -1,5 +1,50 @@
-const ROW_1 = ["Harvard", "Dartmouth", "Northwestern", "Georgia State", "Lafayette", "Lehigh"];
-const ROW_2 = ["Asian University for Women", "Babson", "Denison", "Bucknell", "Aiglon", "Taft"];
+import React from "react";
+
+type School = { name: string; logo?: string };
+
+const LOGO_STYLE: Record<string, React.CSSProperties> = {
+  "Lafayette":     { maxHeight: 36, maxWidth: 120 },
+  "Kenyon":        { maxHeight: 52, maxWidth: 52  },
+  "Bucknell":      { maxHeight: 36, maxWidth: 120 },
+  "Reed College":  { maxHeight: 36, maxWidth: 120 },
+};
+
+const ROW_1: School[] = [
+  { name: "Harvard",                      logo: "harvard.svg"       },
+  { name: "Dartmouth",                    logo: "dartmouth.png"     },
+  { name: "Northwestern",                 logo: "northwestern.svg"  },
+  { name: "Georgia State",                logo: "georgia-state.svg" },
+  { name: "Lafayette",                    logo: "lafayette.png"     },
+  { name: "Lehigh",                       logo: "lehigh.png"        },
+  { name: "Tufts",                        logo: "tufts.png"         },
+  { name: "Kenyon",                       logo: "kenyon.webp"       },
+];
+
+const ROW_2: School[] = [
+  { name: "Asian University for Women",   logo: "auw.png"           },
+  { name: "Babson",                       logo: "babson.svg"        },
+  { name: "Denison",                      logo: "denison.png"       },
+  { name: "Bucknell",                     logo: "bucknell.svg"      },
+  { name: "Aiglon",                       logo: "aiglon.png"        },
+  { name: "Taft",                         logo: "taft.png"          },
+  { name: "Syracuse",                     logo: "syracuse.svg"      },
+  { name: "Reed College",                 logo: "reed-college.png"  },
+];
+
+const SchoolSlot = ({ school }: { school: School }) => (
+  <div className="logo-marquee-slot">
+    {school.logo ? (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src={`/images/logos/${school.logo}`}
+        alt={school.name}
+        style={{ maxHeight: 60, maxWidth: 160, objectFit: "contain", ...LOGO_STYLE[school.name] }}
+      />
+    ) : (
+      <span>{school.name}</span>
+    )}
+  </div>
+);
 
 const AffiliationsPartners = () => (
   <section className="affiliations-partners">
@@ -16,19 +61,15 @@ const AffiliationsPartners = () => (
     <div className="logo-marquee-group">
       <div className="logo-marquee">
         <div className="logo-marquee-track">
-          {[...ROW_1, ...ROW_1].map((name, i) => (
-            <div className="logo-marquee-slot" key={`r1-${i}`}>
-              <span>{name}</span>
-            </div>
+          {[...ROW_1, ...ROW_1].map((school, i) => (
+            <SchoolSlot key={`r1-${i}`} school={school} />
           ))}
         </div>
       </div>
       <div className="logo-marquee logo-marquee--reverse">
         <div className="logo-marquee-track">
-          {[...ROW_2, ...ROW_2].map((name, i) => (
-            <div className="logo-marquee-slot" key={`r2-${i}`}>
-              <span>{name}</span>
-            </div>
+          {[...ROW_2, ...ROW_2].map((school, i) => (
+            <SchoolSlot key={`r2-${i}`} school={school} />
           ))}
         </div>
       </div>
